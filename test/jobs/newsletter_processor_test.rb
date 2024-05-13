f0263c585630d19e45e593b132ed145c1c5a6f6f
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-05-13 11:23:01 +0200

    try to set span type to nil

--- test/jobs/newsletter_processor_test.rb
+++ test/jobs/newsletter_processor_test.rb
@@ -15,7 +15,7 @@ class NewsletterReceiverTest < ActiveSupport::TestCase
     stub_request(:delete, file_url)
       .to_return(status: 204)
 
-    Datadog::CI.trace("newsletter_receiver_test") do
+    Datadog::CI.trace("newsletter_receiver_test", type: nil) do
       assert_difference "Subscription.count", +1 do
         assert_difference "NewsletterSaver.jobs.size", +1 do
           assert_difference("NewsletterSender.count", 1) do
