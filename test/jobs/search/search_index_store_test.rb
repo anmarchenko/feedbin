fc0bc281c7d45cba9c201cc46c7464c452b97f96
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2024-09-19 13:03:56 +0200

    remove test

--- test/jobs/search/search_index_store_test.rb
+++ test/jobs/search/search_index_store_test.rb
@@ -16,21 +16,5 @@ module Search
       entry = Search.client { _1.get(Entry.table_name, id: @entry.id) }
       assert entry["found"]
     end
-
-    test "should percolate entry" do
-      action = nil
-      Sidekiq::Testing.inline! do
-        action = @user.actions.create(feed_ids: [@entry.feed.id], query: "\"#{@entry.title}\"")
-      end
-      Search.client { _1.refresh }
-
-      assert_difference -> { ActionsPerform.jobs.size }, +1 do
-        SearchIndexStore.new.perform("Entry", @entry.id)
-      end
-
-      entry_id, action_ids = ActionsPerform.jobs.first["args"]
-      assert_equal entry_id, @entry.id
-      assert_equal action_ids, [action.id]
-    end
   end
 end
